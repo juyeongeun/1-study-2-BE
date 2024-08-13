551b0d27db8b393bdb833f87f236c22be7a9680c
juyeongeun <80696592+juyeongeun@users.noreply.github.com>
2024-08-13 18:26:48 +0900

    chore: get 검색기능 수정

--- routes/studyCRUD/getStudy.js
+++ routes/studyCRUD/getStudy.js
@@ -8,16 +8,29 @@ const prisma = new PrismaClient();
 router.get(
   "/",
   asyncHandler(async (req, res) => {
-    const { cursor, sortBy, sortOrder } = req.query;
+    const { cursor, sortBy, sortOrder, keyword } = req.query;
 
+    // 정렬 기준 설정
     let orderBy = {};
     if (sortBy) {
       orderBy[sortBy] = sortOrder === "asc" ? "asc" : "desc";
     } else {
-      orderBy = { createAt: "desc" };
+      orderBy = { createAt: "desc" }; // 기본 최신순 정렬
     }
 
+    // 검색 조건 설정
+    const where = keyword
+      ? {
+          OR: [
+            { name: { contains: keyword, mode: "insensitive" } },
+            { studyName: { contains: keyword, mode: "insensitive" } },
+            { content: { contains: keyword, mode: "insensitive" } },
+          ],
+        }
+      : {};
+
     const studies = await prisma.study.findMany({
+      where,
       orderBy,
       skip: cursor ? 1 : 0,
       take: 6,
